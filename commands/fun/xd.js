bcd97827364830359918e28a12125a5acd57154e
CommanderBALA <bala.pasztor.g@gmail.com>
2021-07-20 20:41:10 +0200

    Create xd.js

---
+++ commands/fun/xd.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args, prefix) => {
+
+    message.channel.send({
+        files: [
+          "https://media.discordapp.net/attachments/718871359102517279/727633735981334638/720650612747796499.gif",
+          "https://media.discordapp.net/attachments/718871359102517279/727633736858075166/720650690434957364-1.gif"
+        ]
+      });
+}
+
+module.exports.help = {
+    name: 'xd',
+    aliases: []
+}
