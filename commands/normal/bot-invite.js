8d5514bfff5b0b7e6ec6f6d94fc5a866f21dbc38
CommanderBALA <bala.pasztor.g@gmail.com>
2021-07-21 08:27:09 +0200

    Update bot-invite.js

--- commands/normal/bot-invite.js
+++ commands/normal/bot-invite.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args, prefix) => {
 
-    message.channel.send('https://discord.com/api/oauth2/authorize?client_id=792692203180654592&permissions=8&scope=bot')
+    message.channel.send('Ezen a linken keresztül tudsz behívni a **szerveredre**: '+'https://discord.com/api/oauth2/authorize?client_id=792692203180654592&permissions=8&scope=bot'+' ,megköszönném ha megtennéd!')
 }
 
 module.exports.help = {
