836948e6469632087f0eea33b593895269f02b43
CommanderBALA <bala.pasztor.g@gmail.com>
2021-07-23 12:58:09 +0200

    Update setup.js

--- commands/tickets/setup.js
+++ commands/tickets/setup.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args, prefix) =>{
     message.delete();
     await message.channel.send('A **TICKETS** kategória sikeresen létrehozva! ✅ ').then(msg=>msg.delete({timeout: "3000"}))
+    await message.author.send(`A **TICKETS** kategória sikeresen létrehozva! ✅ ${message.guild.name} `)
     setTimeout(function() {
         message.guild.channels.create("TICKETS", {
           type: "category"
