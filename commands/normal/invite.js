da59679aed703bacd82259c212b8214b6468ff7e
CommanderBALA <bala.pasztor.g@gmail.com>
2021-07-30 18:26:28 +0200

    Update invite.js

--- commands/normal/invite.js
+++ commands/normal/invite.js
@@ -6,7 +6,7 @@ module.exports.run = async (Client, message, args, prefix) => {
 
     .setTitle("Bot-hoz tartozó dolgok")
     .addField("Weboldal: ", "https://commanderbala.github.io/kripton.github.io/html/commands.html", true)
-    .addField("Support szerver: https://discord.com/invite/44VAMDfpHY", "Itt vannak a botról infók: frissitések leállások, stb...")
+    .addField("(Support szerver)[https://discord.com/invite/44VAMDfpHY]", "Itt vannak a botról infók: frissitések leállások, stb...")
     .addField("Bot behívása", "Itt tudod behívni a botot a szerveredre");
 
     message.channel.send(embed)
