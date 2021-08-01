0edb3b0764394758a9c2d2a2dcc230e39fa4479a
CommanderBALA <bala.pasztor.g@gmail.com>
2021-08-01 20:09:43 +0200

    Update setup.js

--- commands/tickets/setup.js
+++ commands/tickets/setup.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args, prefix) =>{
     message.delete();
+    if((!args[0])){
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply('Nincs jogosultságod hogy setupold a ticket-et!').then(msg => msg.delete({timeout: "2000"}));
     await message.channel.send('A **TICKET** kategória sikeresen létrehozva! ✅ ').then(msg=>msg.delete({timeout: "3000"}))
     await message.author.send(`A **TICKET** kategória sikeresen létrehozva! ✅ *(Szerver: ${message.guild.name})* `)
@@ -12,6 +13,11 @@ module.exports.run = async (bot, message, args, prefix) =>{
        type: "category"
         });
       }, 1000);
+    }
+
+    if((args[0]) = 'log'){
+      message.channel.send('XXDD')
+    }
 
 }
 
