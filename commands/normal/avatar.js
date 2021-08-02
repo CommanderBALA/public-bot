e441a1e97baee7e8f81a5ce58ea34a228e9451e8
CommanderBALA <bala.pasztor.g@gmail.com>
2021-08-02 20:08:10 +0200

    Update avatar.js

--- commands/normal/avatar.js
+++ commands/normal/avatar.js
@@ -10,7 +10,7 @@ module.exports.run = async (Client, message, args, prefix) => {
     let avatar = user.displayAvatarURL({size: 4096, dynamic: true})
 
     const embed = new discord.MessageEmbed()
-    .setTitle(`${user.tag}'profilképe!`)
+    .setTitle(`${user.tag}, profilképe!`)
     .setURL(avatar)
     .setImage(avatar)
     .setColor('RANDOM')
