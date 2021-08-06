41bba683f713094fd8ae46d12b8a1314d000b4c6
CommanderBALA <bala.pasztor.g@gmail.com>
2021-08-06 12:04:15 +0200

    Update ping.js

--- commands/normal/ping.js
+++ commands/normal/ping.js
@@ -1,14 +1,26 @@
 const discord = require('discord.js');
 const Client = new discord.Client();
+const cooldown = new Set();
 
 module.exports.run = async (Client, message, args, prefix) => {
 
     if(!message.content.startsWith(prefix)) return;
+       
+    if(cooldown.has(message.author.id)) {
+        message.reply('Please wait 5 seconds between using the command, because you are on cooldown')
+    } else {
 
-    message.channel.send(`Ping lekérése...`).then(msg => {
-        const ping = msg.createdTimestamp - message.createdTimestamp;
-        msg.edit(`A bot pingje: **${ping}**ms!`)
-    })
+        message.channel.send(`Finding the bot ping...`).then(msg => {
+            const ping = msg.createdTimestamp - message.createdTimestamp;
+            msg.edit(`A bot pingje: **${ping}**ms!`)
+        }) 
+
+        cooldown.add(message.author.id);
+        setTimeout(() => {
+            cooldown.delete(message.author.id)
+        }, 5000); // here will be the time in miliseconds 5 seconds = 5000 miliseconds
+    }
+
 }
 
 module.exports.help = {
