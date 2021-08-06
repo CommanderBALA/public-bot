7a558b180caf631a2af7b9c62d871d3fdd534ee6
CommanderBALA <bala.pasztor.g@gmail.com>
2021-08-06 16:36:25 +0200

    Update help.js

--- commands/normal/help.js
+++ commands/normal/help.js
@@ -4,11 +4,9 @@ module.exports.run = async (Client, message, args, prefix) => {
     const embed = new Discord.MessageEmbed()
 
     .setTitle("HELP")
-    .addField("Weboldal: ", "Itt van a parancsok listája", true)
-    .url("https://commanderbala.github.io/kripton.github.io/html/commands.html")
-    .addField("Support szerver ", "Itt vannak a botról infók: frissitések leállások, stb...")
-    .url("https://discord.com/invite/44VAMDfpHY")
-    .addField("Bot behívása", "Itt tudod behívni a botot a szerveredre");
+    .addField("Weboldal: https://commanderbala.github.io/kripton.github.io/html/commands.html", "Itt van a parancsok listája", true)
+    .addField("Support szerver: https://discord.com/invite/44VAMDfpHY ", "Itt vannak a botról infók: frissitések leállások, stb...")
+    .addField("Bot behívása: Hamarosan...", "Itt tudod behívni a botot a szerveredre");
 
     message.channel.send(embed)
 
