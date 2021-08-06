1ebc746892d56424b3cf6b8735afa164ca0da67e
CommanderBALA <bala.pasztor.g@gmail.com>
2021-08-06 19:59:34 +0200

    Create guildname.js

---
+++ commands/NEW/guildname.js
@@ -0,0 +1,30 @@
+const discord = require('discord.js');
+const Client = new discord.Client();
+const cooldown = new Set();
+
+module.exports.run = async (Client, message, args, prefix) => {
+
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.member.hasPermission('MANAGE_GUILD')) return message.reply('Nincs hozzá jogosultságod!')
+    if(!args[0]){
+        message.reply('Nincs megadva új név!');
+    }
+       
+    if(cooldown.has(message.author.id)) {
+        message.reply('Várj 5 másodpercet hogy használhasd újra a parancsot!')
+    } else {
+
+        message.guild.setName(args[0])
+
+        cooldown.add(message.author.id);
+        setTimeout(() => {
+            cooldown.delete(message.author.id)
+        }, 5000); // here will be the time in miliseconds 5 seconds = 5000 miliseconds
+    }
+
+}
+
+module.exports.help = {
+    name: "szervernév",
+    aliases: ['sznév']
+}
