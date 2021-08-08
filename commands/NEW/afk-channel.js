bfff8db1519d1833364f96214236295b1eb839b1
CommanderBALA <bala.pasztor.g@gmail.com>
2021-08-08 21:08:21 +0200

    Update afk-channel.js

--- commands/NEW/afk-channel.js
+++ commands/NEW/afk-channel.js
@@ -3,7 +3,7 @@ const discord = require('discord.js');
 module.exports.run = async (Client, message, args, prefix) => {
     if(!args[0]) return message.reply('Használd: -setAFK [hangcsatorna neve]')
     let afkcsatorna = message.member.guild.channels.cache.find(c => c.name == args[0])
-    if(!afkcsatorna) return message.reply(`Nem találtam ilyen nevű csatornát: ${args[0]}! :x:`);
+    if(!afkcsatorna) return message.reply(`Nem találtam ilyen nevű csatornát: \ ${args[0]} \! :x:`);
     if(afkcsatorna.type='text'||'category'||'news'||'store') return message.reply('Hangcsatornának a nevét add meg!')
     message.guild.setAFKChannel(afkcsatorna)
     message.channel.send(`AFK csatorna sikeresen beállítva: ${afkcsatorna} ! Az AFK időkorlát **5 perc**`)
