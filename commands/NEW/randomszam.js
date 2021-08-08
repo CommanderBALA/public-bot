3e478831cb8fb0ccc41092ea0ffb1bc58d6b4625
CommanderBALA <bala.pasztor.g@gmail.com>
2021-08-08 12:25:02 +0200

    Update randomszam.js

--- commands/NEW/randomszam.js
+++ commands/NEW/randomszam.js
@@ -1,17 +1,14 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (Client, message, args, prefix) => {
-    if(!args[0]) return message.reply('Használd: -randomsz [kisebb szám] [nagyobb szám]')
-    if(!args[1]) return message.reply('Nincs megadva a második szám!')
+    if(!args[0]) return message.reply('Használd: -randomsz [szám]')
 
 const embed = new Discord.MessageEmbed();
     let sum1 = args[0]
-    let sum2 = args[1] -= sum1
-    let sum = Math.floor(Math.random() *sum2) + 1;
-    let ssum = sum += sum1
+    let sum = Math.floor(Math.random() *sum1) + 1;
   
     embed.setColor("#8B9EB5");
-    embed.addField(`Random szám:", "A kisorsolt szám: **${ssum}**!`);
+    embed.addField(`Random szám:`, `A kisorsolt szám: **${sum}**!`);
     embed.setTimestamp();
   
     message.channel.send(embed);
