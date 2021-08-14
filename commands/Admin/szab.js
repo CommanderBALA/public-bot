efdaaf7f31c0bda638c6afaec602e71b873c04fd
CommanderBALA <bala.pasztor.g@gmail.com>
2021-08-14 06:45:41 +0200

    Update szab.js

--- commands/Admin/szab.js
+++ commands/Admin/szab.js
@@ -4,6 +4,8 @@ const pagination = require('discord.js-pagination');
 module.exports.run = async (Client, message, prefix, args) => {
     if(!message.content.startsWith(prefix)) return;
 
+    await message.channel.send('')
+
     const page1 = new discord.MessageEmbed()
     .setTitle('SZABÁLYZAT')
     .setDescription("**1. A felhasználókra vonatkozó szabályok:**\n-    Rangokért keresd a Vezetőséget!\n-    Tilos sértő nicknevet használni!\n\n**2. A szobákra vonatkozó szabályok:**\n-    Ne spamelj!\n-    Ne küldj NSFW tartalmakat!\n-    Ne káromkodj!\n-    Tilos zaklatnod és szidalmaznod a Staff tagokat!\n-    Tilos a hirdetés!\n-    Ne élj vissza a rangoddal!\n-    A csatornákat megfelelően használd!\n\n**3. Staff tagokra vonatkozó szabályok:**\n-    Legyél tisztességes mindenkivel szemben!\n-    Ha kell bannolni/tiltani légy bátor és csináld!\n-    Ne élj vissza a rangoddal!\n\n\n**-    Amennyiben a szabályzatot nem ismered az nem mentesít  annak betartása alól, következményei alól!**\n\n**-    A szabályzat mindenkire érvényes!**\n\n**-    Utolsó módosítás: 2021.08.14.**\n\n**-    Ezek mellett:** **https://discord.com/terms**")
@@ -28,10 +30,10 @@ module.exports.run = async (Client, message, prefix, args) => {
 
     const timeout = '15000'
 
-    pagination(message, pages, emoji, timeout)
+    await pagination(message, pages, emoji, timeout)
 }
 
 module.exports.help = {
-    name: 'pages',
+    name: 'szabályzat',
     aliases: []
 }
