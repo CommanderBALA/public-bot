d2764c9e94b1dbab15ff929b68b92293605b49bf
CommanderBALA <bala.pasztor.g@gmail.com>
2021-08-16 09:55:37 +0200

    .

--- index.js
+++ index.js
@@ -3,9 +3,51 @@ const Discord = require('discord.js')
 const Client = new Discord.Client({disableEveryone: true});
 const fs = require('fs');
 const prefix = ('-');
+const DisTube = require('distube')
 
 Client.aliases = new Discord.Collection();
 Client.commands = new Discord.Collection();
+
+Client.distube = new DisTube(Client, { searchSongs: false, emitNewSongOnly: false });
+
+/// Zene parancs
+Client.distube
+
+.on("playSong", (message, queue, song) => {
+    let playingEmbed = new Discord.MessageEmbed()
+    .setColor("#FFFF00")
+    .setTitle(`🎵 Now Playing 🎵`)
+    .setDescription(`[**${song.name} - ${song.formattedDuration}**](${song.url})`)
+    .setFooter(`Requested by ${song.user.tag}`)
+    message.channel.send(playingEmbed)
+})
+.on("addSong", (message, queue, song) => {
+    let queueEmbed = new Discord.MessageEmbed()
+    .setColor("#FFFF00")
+    .setTitle(`✅ Added to the Queue ✅`)
+    .setDescription(`[**${song.name} - ${song.formattedDuration}**](${song.url})`)
+    .setFooter(`Requested by ${song.user.tag}`)
+    message.channel.send(queueEmbed)
+})
+.on("playList", (message, queue, playlist, song) => {
+
+    message.channel.send(`Play \`${playlist.name}\` playlist (${playlist.songs.length} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\``)
+})
+.on("addList", (message, queue, playlist) => message.channel.send(
+    `Added \`${playlist.name}\` playlist (${playlist.songs.length} songs) to queue`
+))
+// DisTubeOptions.searchSongs = true
+.on("searchResult", (message, result) => {
+    let i = 0;
+    message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`);
+})
+// DisTubeOptions.searchSongs = true
+.on("searchCancel", (message) => message.channel.send(`**Searching canceled!**`))
+.on("error", (message, e) => {
+    console.error(e)
+    message.channel.send("An error encountered: " + e);
+});
+
 
 
 // Welcome message
